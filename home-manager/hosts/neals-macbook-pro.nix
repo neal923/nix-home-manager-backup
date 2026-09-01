38987a244bda39058ad6b635279763e6f7304443
neal <neal.technology@outlook.com>
2026-09-01 10:54:11 +0800

    adjust: adjust neals-macbook-pro config

--- home-manager/hosts/neals-macbook-pro.nix
+++ home-manager/hosts/neals-macbook-pro.nix
@@ -3,7 +3,7 @@
 # 只放本机独有的配置，公共部分在 home-manager/home.nix 里 import。
 { ... }:
 {
-  home.username = "nealwang";
+  home.username = "neal";
   home.homeDirectory = "/Users/neal";
 
   # 本机跑着 privoxy，监听 8118。
